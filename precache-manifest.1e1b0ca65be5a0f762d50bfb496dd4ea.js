32b9e23f172e44a7810cf213ee876d3a03389896
Oleksii Mykhailov <alem.coding@gmail.com>
2020-05-30 14:41:54 +0300

    Updates

---
+++ precache-manifest.1e1b0ca65be5a0f762d50bfb496dd4ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67715386cf1737f05eac4be5b6c9fb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "163f5b02a9c6787f74bf",
+    "url": "./static/css/main.c6abb776.chunk.css"
+  },
+  {
+    "revision": "0d41f43e6a38264b9229",
+    "url": "./static/js/2.8d6d6352.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.8d6d6352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163f5b02a9c6787f74bf",
+    "url": "./static/js/main.b147975f.chunk.js"
+  },
+  {
+    "revision": "bd9b317a4d0439b81bbd",
+    "url": "./static/js/runtime-main.78f69ce3.js"
+  }
+]);
